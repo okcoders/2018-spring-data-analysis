3e33c8c0e89daadd8e21f8abfd432ebacf4987d7
Frank D. Evans <frankdevans@yahoo.com>
2018-03-06 19:42:43 -0600

    Add W1S2 Exercises

---
+++ classes/week1/exercises_s2.R
@@ -0,0 +1,43 @@
+# In-Class Exercises
+
+
+# Factors ------------------------------------------------------------------------------------------
+
+colors <- c("blue", "green", "blue", "red", "red", "orange", NA, "red", "blue")
+
+# How many total elements are there to the colors vector?
+
+
+# Turn colors into a factor and print the structure to see how many levels it contains.
+
+
+
+# Indexing -----------------------------------------------------------------------------------------
+
+data <- c(103, 44, 11, 14, 65, NA, 44, 17, 44, 99, -4, -209)
+
+# Add together the 3rd element and the 7th element of the data.
+
+
+# Subset the data to include only values that are greater than 29.
+
+
+# Make a new version of the data where the NA value no longer exists.
+
+
+# Give me the sum of the 5th to 9th values of the data
+
+
+
+# Combining Functions ------------------------------------------------------------------------------
+
+# How many values in the data are less than 51
+
+
+# Multiply all values in the data less than 25 by 99, and get the sum.
+
+
+# What is the sum of the natural logs of all values in the data equal to or greater than the median?
+
+
+# What percentage of the data is lower than the average?
