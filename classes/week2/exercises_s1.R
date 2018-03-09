438aa978eb2305af0a5ab2367e094beecee3d120
Frank D. Evans <frankdevans@yahoo.com>
2018-03-08 19:05:23 -0600

    Add W2S1 in-class exercises.

---
+++ classes/week2/exercises_s1.R
@@ -0,0 +1,54 @@
+# In-class Exercises: Week 2 Session 1
+
+set.seed(101)
+
+
+# Compound Logic -----------------------------------------------------------------------------------
+
+nums <- c(1:25)
+
+
+# What is the sum of the values in the nums greater than 11 but less than or equal to 19?
+
+
+# What is the mean of all values that are either 22 or less than 8?
+
+
+# What is the standard deviation of values that are either less than or equal to 12 but not
+# including 7?
+
+
+
+# Distributions ------------------------------------------------------------------------------------
+
+# Compute the sum of 329 random normally distributed values from a distribution with a mean
+# of 10 and a standard deviation of 3.
+
+
+# Plot a simple histogram of 2500 values drawn from a beta distribution with shape
+# parameters of 0.9 and 3.1.
+
+
+
+# Data Frames --------------------------------------------------------------------------------------
+
+# Load the built-in iris DataFrame so it can be used, and look at the last 6 rows.
+
+
+# How many rows and columns does the iris data set contain?
+
+
+# What are the unique values of the Species value?
+
+
+# What is the average Sepal Length for flowers from the versicolor species?
+
+
+# Add a new column to the data called "Sepal.Total" that is computed as the sum of the sepal
+# length and width of each flower.
+
+
+# What is the correlation of the new total sepal value to the width of each flower's petal?
+
+
+# Make a small multiples plot of all of the values in iris, including the new one you just added.
